3dc67a78b3ee150bf097539022f33acb37bcc932
esteban95-a <dpachuri@cesde.net>
2025-08-12 11:46:31 -0500

    feat:Increased JavaScript in the login HTML

---
+++ assets/javascript/login.js
@@ -0,0 +1,56 @@
+function mostrarMenu() {
+  console.clear();
+  let opcion = prompt(
+    "Bienvenido a evalix\n" +
+    "Seleccione una opción:\n" +
+    "1. Registro de usuario\n" +
+    "2. Inicio de sesión\n" +
+    "3. Olvidé mi contraseña\n" +
+    "4. cerrar sesión\n" +
+    "\nIngrese el número de la opción:"
+  );
+  if (opcion === "1") {
+    registro();
+  } else if (opcion === "2") {
+    login();
+  } else if (opcion === "3") {
+    recuperarContrasena();
+  } else if (opcion === "4") {
+    cerrarSesion(); 
+  } else {
+    alert("Opción no válida. Intente de nuevo.");
+    mostrarMenu();
+  }
+}
+function registro() {
+  console.clear();
+  let nombre = prompt("Ingrese su nombre completo:");
+  let email = prompt("Ingrese su correo electrónico:");
+  let password = prompt("Ingrese su contraseña:");
+  alert("¡Registro exitoso! Ahora puede iniciar sesión.");
+  mostrarMenu();
+}
+
+
+function login() {
+  console.clear();
+  let tipoCliente = prompt("Ingrese su tipo de cliente ( profesor, alumno):");
+  let email = prompt("Ingrese su correo electrónico:");
+  let password = prompt("Ingrese su contraseña:");
+  if (email && password) {
+    dashboard();
+  } else {
+    alert("Credenciales incorrectas. Intente de nuevo.");
+    mostrarMenu();
+  }
+}
+function recuperarContrasena() {
+  console.clear();
+  let email = prompt("Ingrese su correo electrónico para recuperar su contraseña:");
+  if (email) {
+    alert("Se ha enviado un enlace de recuperación a " + email);
+  } else {
+    alert("Correo electrónico no válido.");
+  }
+  mostrarMenu();
+}
