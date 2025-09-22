dc436440c4a716fa6cd04627ae77098080a2fb9f
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-09-22 16:27:44 -0500

    (develop): Validación de formulario registroAdmin con expresiones regulares

---
+++ assets/javascript/registroAdmi.js
@@ -0,0 +1,65 @@
+import { expresiones } from "./expresiones.js";
+
+let inputs = document.getElementsByClassName("formulario-registro");
+let arregloInputs = [...inputs]
+
+arregloInputs.map((input)=> {
+    input.addEventListener("keyup", (e)=>{
+        switch(e.target.name){
+            case "nombre":
+                if(expresiones.nombre.test(e.target.value)){
+                    e.target.classList.add("input-correcto")
+                    e.target.classList.remove("input-error")
+                } else{
+                    e.target.classList.add("input-error")
+                    e.target.classList.remove("input-correcto")
+                }
+                break;
+            case "correo":
+                if(expresiones.correo.test(e.target.value)){
+                    e.target.classList.add("input-correcto")
+                    e.target.classList.remove("input-error")
+                } else{
+                    e.target.classList.add("input-error")
+                    e.target.classList.remove("input-correcto")
+                }
+                break;
+            case "telefono":
+                if(expresiones.telefono.test(e.target.value)){
+                    e.target.classList.add("input-correcto")
+                    e.target.classList.remove("input-error")
+                } else{
+                    e.target.classList.add("input-error")
+                    e.target.classList.remove("input-correcto")
+                }
+                break;
+            case "direccion":
+                if(expresiones.direccion.test(e.target.value)){
+                    e.target.classList.add("input-correcto")
+                    e.target.classList.remove("input-error")
+                } else{
+                    e.target.classList.add("input-error")
+                    e.target.classList.remove("input-correcto")
+                }
+                break;
+            case "usuario":
+                if(expresiones.nombre.test(e.target.value)){
+                    e.target.classList.add("input-correcto")
+                    e.target.classList.remove("input-error")
+                } else{
+                    e.target.classList.add("input-error")
+                    e.target.classList.remove("input-correcto")
+                }
+                break;
+            case "password":
+                if(expresiones.password.test(e.target.value)){
+                    e.target.classList.add("input-correcto")
+                    e.target.classList.remove("input-error")
+                } else{
+                    e.target.classList.add("input-error")
+                    e.target.classList.remove("input-correcto")
+                }
+                break;
+        }
+    })
+})
